63f6986f5bc083af35eec83eeaef5f2fa1724bd7
viccDev14 <vicsignos@gmail.com>
2025-10-06 18:35:17 -0300

    commit 06/10

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        int [] idades = new int[3];
+
+        idades[0] = 12;
+        idades[1] = 18;
+        idades[2] = 45;
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float e double 0
+        // char '\u0000' ' '
+        // boolean false
+        // String null
+
+        String[] nomes = new String[3];
+        nomes[0] = "vic";
+        nomes[1] = "vic";
+        nomes[2] = "vic";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+
+    }
+}
