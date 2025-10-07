ef16598891a0cfa9a2a387dc66efac9125ca6aed
viccDev14 <vicsignos@gmail.com>
2025-10-07 17:50:37 -0300

    commit 07/10

---
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula08ArraysMultidimensionais01 {
+    public static void main(String[] args) {
+        // 1,2,3,4,5 meses
+        // 31, 28, 30, 31 dias
+        int[][] dias = new int[3][3];
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 31;
+
+        dias[1][0] = 31;
+        dias[1][1] = 28;
+        dias[1][2] = 31;
+
+        for (int i = 0; i < dias.length; i++) {
+            for (int j = 0; j < dias[i].length; j++) {
+                System.out.println(dias[i][j]);
+            }
+        }
+
+        System.out.println("------------------------");
+
+        for (int[] arrayBase: dias) {
+            for (int num: arrayBase) {
+                System.out.println(num);
+            }
+        }
+    }
+}
