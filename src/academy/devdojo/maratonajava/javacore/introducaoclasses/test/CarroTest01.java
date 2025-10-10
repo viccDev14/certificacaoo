55236f6b1e14302249d7a9b3331a2a879b68cc4d
viccDev14 <vicsignos@gmail.com>
2025-10-10 19:28:38 -0300

    commit 10/10

---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/CarroTest01.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro01 = new Carro();
+        Carro carro02 = new Carro();
+
+        carro01.nome = "Mercedes";
+        carro01.modelo = "Sport";
+        carro01.ano = 2000;
+
+        carro02.nome = "BMW";
+        carro02.modelo = "Sport";
+        carro02.ano = 1995;
+
+        carro01 = carro02;
+
+        System.out.println("Carro 1 - Nome: " + carro01.nome + ", Modelo: " + carro01.modelo + ", Ano: " + carro01.ano);
+        System.out.println("Carro 2 - Nome: " + carro02.nome + ", Modelo: " + carro02.modelo + ", Ano: " + carro02.ano);
+
+    }
+}
