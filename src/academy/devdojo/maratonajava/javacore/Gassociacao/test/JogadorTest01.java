25f686239681696af634e94aff7bf846949154aa
viccDev14 <vicsignos@gmail.com>
2025-11-05 17:07:29 -0300

    commit 05/11

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Jogador;
+
+public class JogadorTest01 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Senku");
+        Jogador jogador2 = new Jogador("Ichigo");
+        Jogador jogador3 = new Jogador("Luffy");
+        Jogador[] jogadores = {jogador1, jogador2, jogador3};
+
+        for (Jogador jogador : jogadores) {
+            jogador.imprime();
+        }
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest03.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Jogador;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Time;
+
+public class JogadorTest03 {
+    public static void main(String[] args) {
+        Jogador jogador = new Jogador("Senku");
+        Time time = new Time("Time da Ciencia");
+        Jogador[] jogadores = {jogador};
+        jogador.setTime(time);
+        time.setJogadores(jogadores);
+
+        System.out.println("------Jogador------");
+        jogador.imprime();
+        System.out.println("------Time------");
+        time.imprime();
+    }
+}
