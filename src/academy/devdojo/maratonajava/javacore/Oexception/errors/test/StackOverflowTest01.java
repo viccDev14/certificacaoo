fe0cbce0a8ecd68cd9a6821c909768a660320137
*Vic <*vicsignos@gmail.com*>
2026-01-15 19:26:20 -0300

    commit 15/01

--- src/academy/devdojo/maratonajava/javacore/Oexception/errors/test/StackOverflowTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/errors/test/StackOverflowTest01.java
@@ -1,4 +1,4 @@
-package academy.devdojo.maratonajava.javacore.Oexception.test;
+package academy.devdojo.maratonajava.javacore.Oexception.errors.test;
 
 public class StackOverflowTest01 {
     public static void main(String[] args) {
