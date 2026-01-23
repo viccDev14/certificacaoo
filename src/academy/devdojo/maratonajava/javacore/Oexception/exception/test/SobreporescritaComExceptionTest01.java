9cddab6fd6f5a8b3f5d8eda71afaba16c9a27bf8
*Vic <*vicsignos@gmail.com*>
2026-01-22 21:01:26 -0300

    commit 22/01

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/SobreporescritaComExceptionTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Oexception.exception.test;
+
+import academy.devdojo.maratonajava.javacore.Oexception.exception.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Oexception.exception.dominio.LoginInvalidoException;
+import academy.devdojo.maratonajava.javacore.Oexception.exception.dominio.Pessoa;
+
+public class SobreporescritaComExceptionTest01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        Funcionario funcionario = new Funcionario();
+        try {
+            funcionario.salvar();
+        } catch (LoginInvalidoException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringTest01 {
+    public static void main(String[] args) {
+        String nome = "Vic"; // String constant pool
+        String nome2 = "Vic";
+        nome = nome.concat(" Suane");
+        System.out.println(nome);
+        System.out.println(nome == nome2);
+        String nome3 = new String("Vic");//1 Variavel de referencia, 2 objeto do tipo string, 3 uma string no pool
+        System.out.println(nome2 == nome3);
+        System.out.println(nome2 = nome3.intern());
+
+    }
+}
