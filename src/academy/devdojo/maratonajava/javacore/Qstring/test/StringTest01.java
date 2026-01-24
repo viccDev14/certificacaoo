333b74e7f1613c1b32b9125eb839b7020e132874
*Vic <*vicsignos@gmail.com*>
2026-01-23 22:03:36 -0300

    commit 23/01

--- src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -4,7 +4,7 @@ public class StringTest01 {
     public static void main(String[] args) {
         String nome = "Vic"; // String constant pool
         String nome2 = "Vic";
-        nome = nome.concat(" Suane");
+        nome = nome.concat(" Suane"); // nome += " Suane"
         System.out.println(nome);
         System.out.println(nome == nome2);
         String nome3 = new String("Vic");//1 Variavel de referencia, 2 objeto do tipo string, 3 uma string no pool
