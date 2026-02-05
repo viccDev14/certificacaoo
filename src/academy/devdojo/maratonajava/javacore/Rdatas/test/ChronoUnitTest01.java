c60b81ab6076cc8c4218e6a286da2d7cd0879282
*Vic <*vicsignos@gmail.com*>
2026-02-04 22:10:08 -0300

    commit 04/02

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/ChronoUnitTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class ChronoUnitTest01 {
+    public static void main(String[] args) {
+        LocalDateTime aniversario = LocalDateTime.of(2006, Month.OCTOBER, 14, 14, 0);
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(ChronoUnit.DAYS.between(aniversario, now));
+        System.out.println(ChronoUnit.WEEKS.between(aniversario, now));
+        System.out.println(ChronoUnit.MONTHS.between(aniversario, now));
+        System.out.println(ChronoUnit.YEARS.between(aniversario, now));
+
+    }
+}
