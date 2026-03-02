c23c738cf033cd3db92c8134517c80dc4d247aaf
*Vic <*vicsignos@gmail.com*>
2026-03-02 19:38:39 -0300

    commit 02/03

---
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/NormalizeTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NormalizeTest01 {
+    public static void main(String[] args) {
+        String diretorioProjeto = "home/vic/dev";
+        String arquivoTxt = "../../arquivo.txt";
+        Path path = Paths.get(diretorioProjeto, arquivoTxt);
+        System.out.println(path);
+        System.out.println(path.normalize());
+        Path path2 = Paths.get("/home/./vic/./dev/");
+        System.out.println(path2);
+        System.out.println(path2.normalize());
+    }
+}
